a1b93810338c4639ce5ac6a4c009bc6d2012c3c8
yashar <yasharian1383@gmail.com>
2026-04-27 23:33:41 +0330

    add test

--- test.sh
+++ test.sh
@@ -1,20 +1,33 @@
 cat > /usr/local/etc/xray/config.json << 'EOF'
 {
   "log": {"loglevel": "warning"},
-  "inbounds": [{
-    "tag": "cdn-ws",
-    "port": 443,
-    "protocol": "vless",
-    "settings": {
-      "clients": [{"id": "ba360d83-45d0-44d1-b223-f7b26503d184", "flow": ""}],
-      "decryption": "none"
-    },
-    "streamSettings": {
-      "network": "ws",
-      "wsSettings": {"path": "/api"}
+  "inbounds": [
+    {
+      "port": 80,
+      "protocol": "vless",
+      "settings": {
+        "clients": [
+          {
+            "id": "ba360d83-45d0-44d1-b223-f7b26503d184",
+            "encryption": "none"
+          }
+        ],
+        "decryption": "none"
+      },
+      "streamSettings": {
+        "network": "ws",
+        "security": "none",
+        "wsSettings": {
+          "path": "/api"
+        }
+      }
     }
-  }],
-  "outbounds": [{"protocol": "freedom"}]
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
 }
 EOF
 
@@ -22,7 +35,7 @@ pkill xray
 systemctl stop xray 2>/dev/null
 
 # Run directly with config
-/usr/local/bin/xray run -c /usr/local/etc/xray/config.json
+/usr/local/bin/xray run -c /usr/local/etc/xray/config.json &
 echo waiting 3s until make sure it set up
 sleep 1
 echo  waiting 2s until make sure it set up
