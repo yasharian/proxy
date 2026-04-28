82f056ab1397b486ef05b1eff88225cd721b8737
yashar <yasharian1383@gmail.com>
2026-04-28 14:10:47 +0330

    add test

--- test.sh
+++ test.sh
@@ -1,30 +1,63 @@
 cat > /usr/local/etc/xray/config.json << 'EOF'
 {
-  "log": {"loglevel": "warning"},
+  "log": {
+    "loglevel": "warning"
+  },
   "inbounds": [
     {
-      "port": 80,
+      "tag": "REALITY_IN",
+      "port": 443,
       "protocol": "vless",
       "settings": {
         "clients": [
           {
-            "id": "ba360d83-45d0-44d1-b223-f7b26503d184"
+            "id": "b3f7d18f-6a8f-4d6e-8d78-823c5511906c",
+            "flow": "xtls-rprx-vision"
           }
         ],
         "decryption": "none"
       },
       "streamSettings": {
-        "network": "ws",
-        "security": "none",
-        "wsSettings": {
-          "path": "/api"
+        "network": "tcp",
+        "security": "reality",
+        "realitySettings": {
+          "show": false,
+          "dest": "<your_chosen_domain>:443",
+          "serverNames": ["<your_chosen_domain>"],
+          "privateKey": "0P32Y_rh2gYqFyU3c_CPZ61PVq6UmBNQVqwPevJX-U0",
+          "shortIds": ["", "6ba7b810-9dad-11d1-80b4-00c04fd430c8"],
+          "fingerprint": "chrome",
+          "xver": 0
         }
+      },
+      "sniffing": {
+        "enabled": true,
+        "destOverride": ["http", "tls"]
+      }
+    },
+    {
+      "tag": "SOCKS_IN",
+      "port": 1080,
+      "protocol": "socks",
+      "settings": {
+        "auth": "noauth",
+        "udp": true,
+        "ip": "127.0.0.1"
+      },
+      "sniffing": {
+        "enabled": true,
+        "destOverride": ["http", "tls"]
       }
     }
   ],
   "outbounds": [
     {
-      "protocol": "freedom"
+      "protocol": "freedom",
+      "tag": "direct"
+    },
+    {
+      "protocol": "blackhole",
+      "tag": "block"
     }
   ]
 }
